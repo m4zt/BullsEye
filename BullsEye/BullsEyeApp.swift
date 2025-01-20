e1b341b48c630f0081ab354e12a75a9943c17e77
Marco <marcogough07@gmail.com>
2025-01-19 20:32:27 -0500

    Initial Commit

---
+++ BullsEye/BullsEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullsEyeApp.swift
+//  BullsEye
+//
+//  Created by Mia Gough on 1/19/25.
+//
+
+import SwiftUI
+
+@main
+struct BullsEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
